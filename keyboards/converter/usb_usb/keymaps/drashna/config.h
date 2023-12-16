4588dc524836bd8d5bce7922a8ba214139eaf517
Drashna Jael're <drashna@live.com>
2023-12-15 23:17:46 -0800

    Add USB-USB Converter keymap

---
+++ keyboards/converter/usb_usb/keymaps/drashna/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#define BOOTMAGIC_LITE_COLUMN 9
+#define BOOTMAGIC_LITE_ROW    2
---
+++ keyboards/converter/usb_usb/keymaps/drashna/rules.mk
@@ -0,0 +1,3 @@
+ifeq ($(strip $(KEYBOARD)), converter/usb_usb/pro_micro)
+    F_CPU = 16000000
+endif

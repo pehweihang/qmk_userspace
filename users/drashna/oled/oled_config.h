c73aef7408f39af6f90c09f8dc3a023423b51b50
Drashna Jael're <drashna@live.com>
2024-01-22 19:53:45 -0800

    [OLED] Cleanup oled font code

--- users/drashna/oled/oled_config.h
+++ users/drashna/oled/oled_config.h
@@ -20,21 +20,11 @@
 #define OLED_FONT_END 255
 // #    define OLED_FONT_5X5
 // #    define OLED_FONT_AZTECH
- #    define OLED_FONT_BMPLAIN
+// #    define OLED_FONT_BMPLAIN
 // #    define OLED_FONT_CRACKERS
-// #define OLED_FONT_DEAD_MEAL
+// #    define OLED_FONT_DEAD_MEAL
 // #    define OLED_FONT_EIN
 // #    define OLED_FONT_HISKYF21
 // #    define OLED_FONT_SQUASH
 // #    define OLED_FONT_ZXPIX
 // #    define OLED_FONT_SUPER_DIGG
-
-// #    define OLED_LOGO_BEBOP
-// #    define OLED_LOGO_CORNE
-// #    define OLED_LOGO_GMK_BAD
-// #    define OLED_LOGO_GOTHAM
-// #    define OLED_LOGO_HUE_MANITEE
-// #    define OLED_LOGO_LOOSE
-#define OLED_LOGO_SCIFI
-// #    define OLED_LOGO_SETS3N
-// #    define OLED_LOGO_SKEEB

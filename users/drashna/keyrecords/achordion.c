38548339c9c74fab93de05c5c4d708e929412b9c
Drashna Jael're <drashna@live.com>
2024-01-29 03:03:03 -0800

    [Achordion] Add retro tap support changes

--- users/drashna/keyrecords/achordion.c
+++ users/drashna/keyrecords/achordion.c
@@ -29,6 +29,8 @@ static uint16_t    tap_hold_keycode = KC_NO;
 static uint16_t hold_timer = 0;
 // Eagerly applied mods, if any.
 static uint8_t eager_mods = 0;
+// Flag to determine whether another key is pressed within the timeout.
+static bool pressed_another_key_before_release = false;
 
 #ifdef ACHORDION_STREAK
 // Timer for typing streak
@@ -75,6 +77,14 @@ bool process_achordion(uint16_t keycode, keyrecord_t* record) {
     // Don't process events that Achordion generated.
     if (achordion_state == STATE_RECURSING) {
         return true;
+    }
+
+    // If this is a keypress and if the key is different than the tap-hold key,
+    // this information is saved to a flag to be processed later when the tap-hold
+    // key is released.
+    if (!pressed_another_key_before_release && record->event.pressed && tap_hold_keycode != KC_NO &&
+        tap_hold_keycode != keycode) {
+        pressed_another_key_before_release = true;
     }
 
     // Determine whether the current event is for a mod-tap or layer-tap key.
@@ -124,6 +134,15 @@ bool process_achordion(uint16_t keycode, keyrecord_t* record) {
             tap_hold_record.event.pressed = false;
             // Plumb hold release event.
             recursively_process_record(&tap_hold_record, STATE_RELEASED);
+        } else if (!pressed_another_key_before_release) {
+            // No other key was pressed between the press and release of the tap-hold
+            // key, simulate a hold and then a release without waiting for Achordion
+            // timeout to end.
+            dprintln("Achordion: Key released. Simulating hold and release.");
+            settle_as_hold();
+            tap_hold_record.event.pressed = false;
+            // Plumb hold release event.
+            recursively_process_record(&tap_hold_record, STATE_RELEASED);
         } else {
             dprintf("Achordion: Key released.%s\n", eager_mods ? " Clearing eager mods." : "");
             if (is_mt) {
@@ -132,6 +151,9 @@ bool process_achordion(uint16_t keycode, keyrecord_t* record) {
         }
 
         achordion_state = STATE_RELEASED;
+        // The tap-hold key is released, clear the related keycode and the flag.
+        tap_hold_keycode                   = KC_NO;
+        pressed_another_key_before_release = false;
         return false;
     }
 
@@ -218,32 +240,11 @@ bool achordion_opposite_hands(const keyrecord_t* tap_hold_record, const keyrecor
 // "held" only when it and the other key are on opposite hands.
 __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
                                            uint16_t other_keycode, keyrecord_t* other_record) {
-    // Exceptionally consider the following chords as holds, even though they
-    // are on the same hand in Dvorak.
-    switch (tap_hold_keycode) {
-        case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-        case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
-            return true;
-    }
-
-    // Also allow same-hand holds when the other key is in the rows below the
-    // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
-    if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) {
-        return true;
-    }
-
-    // Otherwise, follow the opposite hands rule.
-
     return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
 // By default, the timeout is 1000 ms for all keys.
 __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
-    if (layer_state_is(_MOUSE)) {
-        return 0;
-    }
-
-    // Otherwise, follow the opposite hands rule.
     return 1000;
 }
 
@@ -256,5 +257,4 @@ __attribute__((weak)) bool achordion_eager_mod(uint8_t mod) {
 __attribute__((weak)) uint16_t achordion_streak_timeout(uint16_t tap_hold_keycode) {
     return 100; // Default of 100 ms.
 }
-
 #endif

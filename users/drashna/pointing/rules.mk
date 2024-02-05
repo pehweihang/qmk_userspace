bd1ece750daff34199cf6af25b1d124ad38ef0ec
Drashna Jael're <drashna@live.com>
2024-02-04 17:36:15 -0800

    Rename configs to just config.h

--- users/drashna/pointing/rules.mk
+++ users/drashna/pointing/rules.mk
@@ -3,7 +3,7 @@ ifeq ($(strip $(POINTING_DEVICE_ENABLE)), yes)
     ifeq ($(strip $(CUSTOM_POINTING_DEVICE)), yes)
         SRC += $(USER_PATH)/pointing/pointing.c
         OPT_DEFS += -DCUSTOM_POINTING_DEVICE
-        CONFIG_H += $(USER_PATH)/pointing/pointing_config.h
+        CONFIG_H += $(USER_PATH)/pointing/config.h
     endif
     POINTING_DEVICE_MOUSE_JIGGLER_ENABLE ?= yes
     ifeq ($(strip $(POINTING_DEVICE_MOUSE_JIGGLER_ENABLE)), yes)

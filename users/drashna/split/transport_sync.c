5459c1fd366017729859589ef93a1f29e99ef0fa
Drashna Jael're <drashna@live.com>
2024-02-17 16:31:45 -0800

    [OLED] Remove deprcated tap_toggle variable

--- users/drashna/split/transport_sync.c
+++ users/drashna/split/transport_sync.c
@@ -17,9 +17,6 @@ extern unicode_config_t unicode_config;
 extern audio_config_t audio_config;
 extern bool           delayed_tasks_run;
 #endif
-#if defined(POINTING_DEVICE_ENABLE) && defined(KEYBOARD_handwired_tractyl_manuform)
-extern bool tap_toggling;
-#endif
 #ifdef SWAP_HANDS_ENABLE
 extern bool swap_hands;
 #endif

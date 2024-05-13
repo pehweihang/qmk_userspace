57b48ca9afde0c7fc5038ced721f47b7a7f43f96
Drashna Jael're <drashna@live.com>
2024-05-13 11:33:42 -0700

    [Voyager] Add I2C EEPROM

---
+++ keyboards/zsa/voyager/keymaps/drashna/config.h
@@ -0,0 +1,6 @@
+// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#define EEPROM_I2C_24LC256

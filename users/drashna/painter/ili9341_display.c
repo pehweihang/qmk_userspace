b9a241a9406524b332e62db5e08d8d6eda7defe2
Drashna Jael're <drashna@live.com>
2024-07-23 13:02:23 -0700

    [Painter] Updates and tweaks

--- users/drashna/painter/ili9341_display.c
+++ users/drashna/painter/ili9341_display.c
@@ -1,5 +1,6 @@
 #include "drashna.h"
 #include <qp.h>
+#include "handwired/tractyl_manuform/5x6_right/f405_coreboard/config.h"
 #include "keycode_config.h"
 #include "painter/qp_comms.h"
 #include "qp_ili9341.h"
@@ -60,7 +61,7 @@ void draw_ui_user(void) {
 #endif
 
 #ifdef POINTING_DEVICE_ENABLE
-    static uint16_t last_cpi[ILI9341_NUM_DEVICES] = {0xFFFF};
+    static uint16_t last_cpi   = {0xFFFF};
     uint16_t        curr_cpi   = charybdis_get_pointer_sniping_enabled() ? charybdis_get_pointer_sniping_dpi()
                                                                          : charybdis_get_pointer_default_dpi();
     bool            cpi_redraw = false;
@@ -132,14 +133,12 @@ void draw_ui_user(void) {
     }
 #endif
 
-#ifdef DEBUG_MATRIX_SCAN_RATE_ENABLE
     bool            scan_redraw      = false;
     static uint32_t last_scan_update = 0;
     if (timer_elapsed32(last_scan_update) > 125) {
         last_scan_update = timer_read32();
         scan_redraw      = true;
     }
-#endif
 
 #if defined(RGB_MATRIX_ENABLE) || defined(RGBLIGHT_ENABLE)
     bool            rgb_effect_redraw = false;
@@ -170,7 +169,7 @@ void draw_ui_user(void) {
         //     lock_scrl_off, curr_hue, 255, last_led_state.scroll_lock ? 255 : 32, curr_hue, 255, 0);
         // }
 
-        ypos += font->line_height + 4;
+        // ypos += font->line_height + 4;
         if (hue_redraw || wpm_redraw) {
             static int max_wpm_xpos = 0;
             xpos                    = 5;
@@ -182,7 +181,6 @@ void draw_ui_user(void) {
             qp_rect(ili9341_display, xpos, ypos, max_wpm_xpos, ypos + font->line_height, 0, 0, 0, true);
         }
 
-#ifdef DEBUG_MATRIX_SCAN_RATE_ENABLE
         if (hue_redraw || scan_redraw) {
             static int max_scans_xpos = 0;
             xpos                      = 50;
@@ -193,7 +191,6 @@ void draw_ui_user(void) {
             }
             qp_rect(ili9341_display, xpos, ypos, max_scans_xpos, ypos + font->line_height, 0, 0, 0, true);
         }
-#endif
 
 #ifdef POINTING_DEVICE_ENABLE
         if (hue_redraw || cpi_redraw) {
@@ -218,7 +215,7 @@ void draw_ui_user(void) {
         if (hue_redraw || ds_state_redraw) {
             static int max_dss_xpos = 0;
             xpos                    = 5;
-            const char* buf         = charybdis_get_pointer_dragscroll_enabled() ? "DS:ACTIVE" : "DS:OFF";
+            const char* buf         = "DRAG SCROLL";
             xpos += qp_drawtext_recolor(ili9341_display, xpos, ypos, font, buf,
                                         charybdis_get_pointer_dragscroll_enabled() ? 153 : 255, 255, 255,
                                         charybdis_get_pointer_dragscroll_enabled() ? 153 : 255, 255, 0);
@@ -230,8 +227,8 @@ void draw_ui_user(void) {
 
         if (hue_redraw || am_state_redraw) {
             static int max_ams_xpos = 0;
-            xpos                    = 50;
-            const char* buf         = get_auto_mouse_enable() ? "AM:ACTIVE" : "AM:OFF";
+            xpos += 10;
+            const char* buf = "Auto Layer";
             xpos += qp_drawtext_recolor(ili9341_display, xpos, ypos, font, buf, get_auto_mouse_enable() ? 34 : 255, 255,
                                         255, charybdis_get_pointer_sniping_enabled() ? 34 : 255, 255, 0);
             if (max_ams_xpos < xpos) {
@@ -242,8 +239,8 @@ void draw_ui_user(void) {
 
         if (hue_redraw || sp_state_redraw) {
             static int max_sps_xpos = 0;
-            xpos                    = 110;
-            const char* buf         = charybdis_get_pointer_sniping_enabled() ? "SN:ACTIVE" : "SN:OFF";
+            xpos += 10;
+            const char* buf = "Sniping";
             xpos += qp_drawtext_recolor(ili9341_display, xpos, ypos, font, buf,
                                         charybdis_get_pointer_sniping_enabled() ? 153 : 255, 255, 255,
                                         charybdis_get_pointer_sniping_enabled() ? 153 : 255, 255, 0);
@@ -282,7 +279,6 @@ void draw_ui_user(void) {
             qp_rect(ili9341_display, xpos, ypos, max_bpm_xpos, ypos + font->line_height, 0, 0, 0, true);
         }
 
-#if 0
         ypos += font->line_height + 2;
         static user_runtime_config_t last_user_state = {0};
         if (hue_redraw || last_user_state.raw != user_state.raw) {
@@ -307,21 +303,13 @@ void draw_ui_user(void) {
             }
             qp_rect(ili9341_display, xpos, ypos, max_upm_xpos, ypos + font->line_height, 0, 0, 0, true);
         }
-#endif
 
+#if defined(RGB_MATRIX_ENABLE)
         ypos += font->line_height + 4;
-        if (hue_redraw
-#if defined(RGB_MATRIX_ENABLE) || defined(RGBLIGHT_ENABLE)
-            || rgb_effect_redraw
-#endif
-        ) {
+        if (hue_redraw || rgb_effect_redraw) {
             static int max_rgb_xpos = 0;
             xpos                    = 5;
-#if defined(RGB_MATRIX_ENABLE)
-            snprintf(buf, sizeof(buf), "RGB: %s", rgb_matrix_name(curr_effect));
-#elif defined(RGBLIGHT_ENABLE)
-            snprintf(buf, sizeof(buf), "RGB: %s", rgblight_name(curr_effect));
-#endif
+            snprintf(buf, sizeof(buf), "RGB Matrix: %s", rgb_matrix_name(curr_effect));
             for (int i = 5; i < sizeof(buf); ++i) {
                 if (buf[i] == 0)
                     break;
@@ -339,7 +327,32 @@ void draw_ui_user(void) {
             }
             qp_rect(ili9341_display, xpos, ypos, max_rgb_xpos, ypos + font->line_height, 0, 0, 0, true);
         }
+#endif
 
+#if defined(RGBLIGHT_ENABLE)
+        ypos += font->line_height + 4;
+        if (hue_redraw || rgb_effect_redraw) {
+            static int max_rgb_xpos = 0;
+            xpos                    = 5;
+            snprintf(buf, sizeof(buf), "RGB Light: %s", rgblight_name(curr_effect));
+            for (int i = 5; i < sizeof(buf); ++i) {
+                if (buf[i] == 0)
+                    break;
+                else if (buf[i] == '_')
+                    buf[i] = ' ';
+                else if (buf[i - 1] == ' ')
+                    buf[i] = toupper(buf[i]);
+                else if (buf[i - 1] != ' ')
+                    buf[i] = tolower(buf[i]);
+            }
+
+            xpos += qp_drawtext_recolor(ili9341_display, xpos, ypos, font, buf, curr_hue, 255, 255, curr_hue, 255, 0);
+            if (max_rgb_xpos < xpos) {
+                max_rgb_xpos = xpos;
+            }
+            qp_rect(ili9341_display, xpos, ypos, max_rgb_xpos, ypos + font->line_height, 0, 0, 0, true);
+        }
+#endif
         ypos += font->line_height + 4;
         if (hue_redraw || dl_state_redraw || layer_state_redraw) {
             const char* dl_name = "unknown";
@@ -451,9 +464,9 @@ void keyboard_post_init_quantum_painter(void) {
 #endif
     wait_ms(150);
 
-    ili9341_display = qp_ili9341_make_spi_device(240, 320, DISPLAY_CS_PIN_ILI9341, DISPLAY_DC_PIN_ILI9341,
-                                                 DISPLAY_RST_PIN_ILI9341, 4, 0);
-    init_and_clear(ili9341_display, QP_ROTATION_0);
+    ili9341_display =
+        qp_ili9341_make_spi_device(240, 320, DISPLAY_CS_PIN, DISPLAY_DC_PIN, DISPLAY_RST_PIN, DISPLAY_SPI_DIVIDER, 0);
+    init_and_clear(ili9341_display, QP_ROTATION_180);
     qp_power(ili9341_display, true);
 
     wait_ms(50);
@@ -498,6 +511,7 @@ void shutdown_quantum_painter(void) {
 
 #ifdef KEYLOGGER_ENABLE
 // clang-format off
+#ifndef OLED_ENABLE
 static const char PROGMEM code_to_name[256] = {
 //   0    1    2    3    4    5    6    7    8    9    A    B    c    D    E    F
     ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',  // 0x
@@ -517,6 +531,7 @@ static const char PROGMEM code_to_name[256] = {
     'C', 'S', 'A', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',  24,  26,  24,  // Ex
      25,0x9D,0x9D,0x9D,0x9D,0x9D,0x9D,0x9D,0x9D,  24,  25,  27,  26, ' ', ' ', ' '   // Fx
 };
+#endif
 // clang-format on
 
 /**
@@ -525,7 +540,7 @@ static const char PROGMEM code_to_name[256] = {
  * @param keycode Keycode pressed from switch matrix
  * @param record keyrecord_t data structure
  */
-void add_keylog(uint16_t keycode, keyrecord_t* record) {
+static void add_keylog(uint16_t keycode, keyrecord_t* record) {
     if (keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) {
         keycode = QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
     } else if (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX) {

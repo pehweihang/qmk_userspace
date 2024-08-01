9f1e8545aa4d757bb80d7f31c6c9e9327151c862
Drashna Jael're <drashna@live.com>
2024-08-01 11:34:49 -0700

    [Autocorrect] Enable raw output

--- users/drashna/drashna.c
+++ users/drashna/drashna.c
@@ -133,12 +133,16 @@ float autocorrect_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
 #    endif
 // 2 strings, 2q chars each + null terminator. max autocorrect length is 19 chars but 128px/6 supports 21 chars
 char autocorrected_str[2][21] = {"    automatically\0", "      corrected\0"};
+char autocorrected_str_raw[2][21] = {"automatically\0", "corrected\0"};
 bool autocorrect_str_has_changed = false;
 
 bool apply_autocorrect(uint8_t backspaces, const char *str, char *typo, char *correct) {
     if (is_gaming_layer_active(layer_state)) {
         return false;
     }
+    strncpy(autocorrected_str_raw[0], typo, sizeof(autocorrected_str_raw[0]) - 1);
+    strncpy(autocorrected_str_raw[1], correct, sizeof(autocorrected_str_raw[1]) - 1);
+
     center_text(typo, autocorrected_str[0], sizeof(autocorrected_str[0]) - 1);
     center_text(correct, autocorrected_str[1], sizeof(autocorrected_str[1]) - 1);
     // printf("Autocorrected %s to %s (original: %s)\n", typo, correct, str);

6b20d30921b0284f406888c6cd88225d10b4bd32
Drashna Jael're <drashna@live.com>
2024-08-25 15:16:23 -0700

    [TractylM] Add f405 configuration

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/mcuconf.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/mcuconf.h
@@ -1,9 +1,16 @@
+// Copyright 2024 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #pragma once
 
 #include_next <mcuconf.h>
 
-#undef STM32_WDG_USE_IWDG
-#define STM32_WDG_USE_IWDG TRUE
+#if HAL_USE_WDG == TRUE
+#    undef STM32_WDG_USE_IWDG
+#    define STM32_WDG_USE_IWDG TRUE
+#endif
 
-#undef STM32_I2C_BUSY_TIMEOUT
-#define STM32_I2C_BUSY_TIMEOUT 10
+#if HAL_USE_I2C == TRUE
+#    undef STM32_I2C_BUSY_TIMEOUT
+#    define STM32_I2C_BUSY_TIMEOUT 10
+#endif
--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
@@ -40,8 +40,27 @@ ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f411)
     HAPTIC_DRIVER                    = drv2605l
     RTC_DRIVER                       = ds3231
     WATCHDOG_ENABLE                  = yes
+    OLED_ENABLE                      = yes
+    RTC_ENABLE                       = yes
 endif
+ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f405)
+    BOOTLOADER                       = tinyuf2
+    AUTOCORRECT_ENABLE               = yes
+    OVERLOAD_FEATURES                = yes
+    RTC_ENABLE                       = yes
+    RTC_DRIVER                       = vendor
+    WATCHDOG_ENABLE                  = yes
+    OLED_ENABLE                      = no
+    RGB_MATRIX_ENABLE                = yes
+    RGBLIGHT_DRIVER                  = custom
+    LAYER_MAP_ENABLE                 = yes
 
+    BACKLIGHT_ENABLE                 = yes
+    QUANTUM_PAINTER_ENABLE           = yes
+    QUANTUM_PAINTER_DRIVERS          += ili9341_spi
+    CUSTOM_QUANTUM_PAINTER_ENABLE    = yes
+    DISPLAY_KEYLOGGER_ENABLE         = yes
+endif
 ifeq ($(strip $(OVERLOAD_FEATURES)), yes)
     AUDIO_ENABLE                     = yes
     CAPS_WORD_ENABLE                 = yes
@@ -49,13 +68,11 @@ ifeq ($(strip $(OVERLOAD_FEATURES)), yes)
     CUSTOM_UNICODE_ENABLE            = yes
     ENCODER_ENABLE                   = yes
     ENCODER_MAP_ENABLE               = yes
-    OLED_ENABLE                      = yes
     RGBLIGHT_ENABLE                  = yes
-    RGBLIGHT_STARTUP_ANIMATION       = yes
+    RGBLIGHT_STARTUP_ANIMATION       = no
     TAP_DANCE_ENABLE                 = yes
     SWAP_HANDS_ENABLE                = yes
     WPM_ENABLE                       = yes
-    RTC_ENABLE                       = yes
     SENTENCE_CASE_ENABLE             = yes
     DISPLAY_KEYLOGGER_ENABLE         = yes
 endif

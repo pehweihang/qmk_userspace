3b2bb7954e2e9f3468b6351c5445dd04d6618405
Drashna Jael're <drashna@live.com>
2024-08-26 17:33:06 -0700

    Add Konami Code -- thanks tzarc

--- users/drashna/keyrecords/process_records.c
+++ users/drashna/keyrecords/process_records.c
@@ -1,4 +1,5 @@
 // Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// Copyright 2018-2024 Nick Brassel (@tzarc)
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "process_records.h"
@@ -18,6 +19,69 @@
 #endif
 
 uint16_t copy_paste_timer;
+
+static void konami_code_handler(void) {
+    dprintf("Konami code entered\n");
+    wait_ms(50);
+    reset_keyboard();
+}
+
+static bool process_record_konami_code(uint16_t keycode, keyrecord_t *record) {
+    static uint8_t        konami_index          = 0;
+    static const uint16_t konami_code[] PROGMEM = {KC_UP,   KC_UP,    KC_DOWN, KC_DOWN, KC_LEFT, KC_RIGHT,
+                                                   KC_LEFT, KC_RIGHT, KC_B,    KC_A,    KC_ENTER};
+
+    if (!record->event.pressed) {
+        switch (keycode) {
+            case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+            case QK_DEF_LAYER ... QK_DEF_LAYER_MAX:
+            case QK_TOGGLE_LAYER ... QK_TOGGLE_LAYER_MAX:
+            case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX:
+            case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
+                // Messing with layers, ignore but don't reset the counter.
+                break;
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+                return process_record_konami_code(QK_MOD_TAP_GET_TAP_KEYCODE(keycode), record);
+            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+                return process_record_konami_code(QK_LAYER_TAP_GET_TAP_KEYCODE(keycode), record);
+            case QK_SWAP_HANDS ... QK_SWAP_HANDS_MAX:
+                return process_record_konami_code(QK_SWAP_HANDS_GET_TAP_KEYCODE(keycode), record);
+            case KC_KP_ENTER:
+            case KC_RETURN:
+            case QK_SPACE_CADET_RIGHT_SHIFT_ENTER:
+                return process_record_konami_code(KC_ENTER, record);
+            case KC_UP:
+            case KC_DOWN:
+            case KC_LEFT:
+            case KC_RIGHT:
+            case KC_B:
+            case KC_A:
+            case KC_ENTER:
+                if (keycode == pgm_read_word(&konami_code[konami_index])) {
+                    dprintf("Konami code: key released: %s\n", key_name(keycode, false));
+                    konami_index++;
+                    if (konami_index == ARRAY_SIZE(konami_code)) {
+                        konami_index = 0;
+                        konami_code_handler();
+                    }
+                } else {
+                    if (konami_index) {
+                        dprintf("Konami code: reset\n");
+                    }
+                    konami_index = 0;
+                }
+                break;
+            default:
+                if (konami_index) {
+                    dprintf("Konami code: reset\n");
+                }
+                konami_index = 0;
+                break;
+        }
+    }
+    return true;
+}
+
 // Defines actions tor my global custom keycodes. Defined in drashna.h file
 // Then runs the _keymap's record handier if not processed here
 
@@ -77,6 +141,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
 #endif
+
+    if (!process_record_konami_code(keycode, record)) {
+        return false;
+    }
+
     if (!(process_record_keymap(keycode, record) && process_record_secrets(keycode, record)
 #ifdef DISPLAY_DRIVER_ENABLE
           && process_record_display_driver(keycode, record)

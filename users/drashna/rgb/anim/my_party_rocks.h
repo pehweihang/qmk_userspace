ae41b8accdee6057c41c4fd6a1324b24d3fb2072
Drashna Jael're <drashna@live.com>
2024-09-02 07:41:39 -0700

    [RGB Matrix] Fixup custom animations

--- users/drashna/rgb/anim/my_party_rocks.h
+++ users/drashna/rgb/anim/my_party_rocks.h
@@ -1,4 +1,4 @@
-/* Copyright 2022 3araht
+/* Copyright 2022 3araht */
 // Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -11,7 +11,9 @@ bool my_party_rocks(effect_params_t* params) {
     HSV hsv = {rand() & 0xFF, rand() & 0xFF, rgb_matrix_config.hsv.v};
     RGB rgb = rgb_matrix_hsv_to_rgb(hsv);
     // rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-    rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
+    for (uint8_t i = led_min; i < led_max; i++) {
+        rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+    }
     return rgb_matrix_check_finished_leds(led_max);
 }
 #    endif

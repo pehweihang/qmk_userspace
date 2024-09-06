322d173fdbeda5682679d8741869839713095721
Drashna Jael're <drashna@live.com>
2024-09-06 15:53:08 -0700

    [RGB Matrix] Enable Alpha Mods

--- users/drashna/rgb/rgb_matrix_config.h
+++ users/drashna/rgb/rgb_matrix_config.h
@@ -63,6 +63,7 @@
 // RGB Matrix Animation modes. Explicitly enabled
 // For full list of effects, see:
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
 #    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #    define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
 #    define ENABLE_RGB_MATRIX_BREATHING

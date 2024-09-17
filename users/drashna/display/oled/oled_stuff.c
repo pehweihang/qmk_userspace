ae2735528424646d7d6654c481c5ea90d8283cdd
Drashna Jael're <drashna@live.com>
2024-09-17 08:21:18 -0700

    [OLED] Render from memory and align to 13 character width

--- users/drashna/display/oled/oled_stuff.c
+++ users/drashna/display/oled/oled_stuff.c
@@ -691,7 +691,9 @@ void render_unicode_mode(uint8_t col, uint8_t line) {
 #if defined(CUSTOM_UNICODE_ENABLE) && defined(UNICODE_COMMON_ENABLE)
     oled_set_cursor(col, line);
     oled_write_P(PSTR("Unicode:"), false);
-    oled_write(unicode_mode_str[unicode_typing_mode], false);
+    char buf[14] = {0};
+    snprintf(buf, sizeof(buf), "%13s", unicode_mode_str[unicode_typing_mode]);
+    oled_write(buf, false);
 #endif
 }
 
@@ -699,7 +701,9 @@ void render_unicode_mode_small(uint8_t col, uint8_t line, bool invert) {
 #if defined(CUSTOM_UNICODE_ENABLE) && defined(UNICODE_COMMON_ENABLE)
     oled_set_cursor(col, line);
     oled_write_P(PSTR("UC"), invert);
-    oled_write(unicode_mode_str[unicode_typing_mode], invert);
+    char buf[14] = {0};
+    snprintf(buf, sizeof(buf), "%13s", unicode_mode_str[unicode_typing_mode]);
+    oled_write(buf, false);
 #endif
 }
 

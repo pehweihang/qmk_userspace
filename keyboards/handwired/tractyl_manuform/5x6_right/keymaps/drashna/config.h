7ad70dc139fdfaaba8719cfb0f082c7e77d1c000
Drashna Jael're <drashna@live.com>
2024-09-20 07:48:26 -0700

    [TractylM] Significantly increase user eeprom size for f405

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/config.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/config.h
@@ -34,6 +34,7 @@
 #    undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
 #    define WEAR_LEVELING_BACKING_SIZE    (0x4000)
+#    define EECONFIG_USER_DATA_SIZE       (0x3800)
 #elif defined(KEYBOARD_handwired_tractyl_manuform_5x6_right_f411)
 #    define EECONFIG_USER_DATA_SIZE 4096
 #endif

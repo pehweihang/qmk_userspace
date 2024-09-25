1f03b36a250061e2fe8bbb8fd35d5d614a71a9ab
Drashna Jael're <drashna@live.com>
2024-09-25 12:49:08 -0700

    [EEPROM] Lets force init eeprom if not valid

--- users/drashna/callbacks.c
+++ users/drashna/callbacks.c
@@ -48,6 +48,9 @@ __attribute__((weak)) void keyboard_pre_init_keymap(void) {}
 void                       keyboard_pre_init_user(void) {
     print_set_sendchar(drashna_sendchar);
     eeconfig_read_user_datablock(&userspace_config);
+    if (!eeconfig_is_user_datablock_valid() || !userspace_config.check) {
+        eeconfig_init_user();
+    }
 
     keyboard_pre_init_keymap();
 }
@@ -278,6 +281,7 @@ void                       led_set_user(uint8_t usb_led) {
 __attribute__((weak)) void eeconfig_init_keymap(void) {}
 void                       eeconfig_init_user(void) {
     memset(&userspace_config, 0, sizeof(userspace_config_t));
+    userspace_config.check            = true;
     userspace_config.rgb.layer_change = true;
 #if defined(OLED_ENABLE)
     userspace_config.oled.brightness = OLED_BRIGHTNESS;

081a0b7196f187b448cf2dcb388e3f3b0097ea44
Drashna Jael're <drashna@live.com>
2024-09-26 12:08:26 -0700

    [Sendchar] Fix compilation issues with AVR because .... AVR

--- users/drashna/sendchar.h
+++ users/drashna/sendchar.h
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <stdint.h>
+#ifdef __AVR__
+#    include_next "sendchar.h"
+#endif
 
 extern uint32_t sendchar_timer;
 int8_t          drashna_sendchar(uint8_t c);

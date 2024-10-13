d2371fbf1ccc1cc6e6b298525f5f9376a3181b2c
Drashna Jael're <drashna@live.com>
2024-10-12 20:25:16 -0700

    Fine, add oled stuff back

--- users/drashna/drashna.h
+++ users/drashna/drashna.h
@@ -16,6 +16,9 @@
 #if defined(RGB_MATRIX_ENABLE)
 #    include "rgb/rgb_matrix_stuff.h"
 #endif // defined(RGB_MATRIX_ENABLE)
+#if defined(CUSTOM_OLED_DRIVER)
+#    include "display/oled/oled_stuff.h"
+#endif // CUSTOM_OLED_DRIVER
 
 void     matrix_scan_rate_task(void);
 uint32_t get_matrix_scan_rate(void);
